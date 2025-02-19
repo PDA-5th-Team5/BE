cd8d9d0c4daf637a39fab09837ec94df316d0425
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-02-20 08:50:20 +0900

    first commit
    first commit

---
+++ src/snowflake-service/src/test/java/com/pda/snowflakeservice/SnowflakeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pda.snowflakeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowflakeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
