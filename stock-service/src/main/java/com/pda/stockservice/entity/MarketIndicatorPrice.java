a07fe10c7a905bc44af29e964e07837057402798
ehdms1115@naver.com <ehdms1115@naver.com>
2025-02-25 17:50:12 +0900

    feat(#24): MarketIndicatorPrice Entity

---
+++ stock-service/src/main/java/com/pda/stockservice/entity/MarketIndicatorPrice.java
@@ -0,0 +1,29 @@
+package com.pda.stockservice.entity;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MarketIndicatorPrice {
+
+    public static class MarketIndicatorPriceId implements Serializable {
+        private String Market;
+        private LocalDate date;
+    }
+
+    @EmbeddedId
+    private MarketIndicatorPriceId id;
+    private Integer price;
+
+
+}
