2d3b5b3ac94bfd083dedbb00e898ab52e560d7e3
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-02-25 09:43:19 +0900

    fix: 멀티 모듈 구조 변경 및 의존성 수정

---
+++ snowflake-service/src/main/java/com/pda/snowflakeservice/SnowflakeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pda.snowflakeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowflakeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnowflakeServiceApplication.class, args);
+    }
+
+}
