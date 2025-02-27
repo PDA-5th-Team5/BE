e9732c2bbeb51a0a3e46496b9dcd241e9f2980aa
ehdms1115@naver.com <ehdms1115@naver.com>
2025-02-27 10:40:54 +0900

    feat(38):관심종목 추가/삭제 API구현

--- stock-service/src/main/java/com/pda/stockservice/service/StockService.java
+++ stock-service/src/main/java/com/pda/stockservice/service/StockService.java
@@ -4,5 +4,7 @@ import com.pda.stockservice.dto.response.StockInfoResponseDTO;
 
 public interface StockService {
     StockInfoResponseDTO getStocks(Short stockId);
-
+    //로그인기능구현 후 useId추가 예정
+    void addFavoriteStock(Short stockId);
+    void deleteFavoriteStock(Short stockId);
 }
--- stock-service/src/main/java/com/pda/stockservice/service/StockServiceImpl.java
+++ stock-service/src/main/java/com/pda/stockservice/service/StockServiceImpl.java
@@ -1,7 +1,9 @@
 package com.pda.stockservice.service;
 
 import com.pda.stockservice.dto.response.StockInfoResponseDTO;
+import com.pda.stockservice.entity.FavoriteStock;
 import com.pda.stockservice.entity.Stock;
+import com.pda.stockservice.repository.FavoriteStockRepository;
 import com.pda.stockservice.repository.StockRepository;
 import com.pda.utilservice.response.ApiResponse;
 import com.pda.utilservice.response.code.resultCode.ErrorStatus;
@@ -15,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class StockServiceImpl implements StockService {
 
     private final StockRepository stockRepository;
+    private final FavoriteStockRepository favoriteStockRepository;
 
     // 개별 종목 정보 조회
     @Transactional(readOnly = true)
@@ -23,4 +26,38 @@ public class StockServiceImpl implements StockService {
                 .orElseThrow(() -> new StockHandler(ErrorStatus.STOCK_NOT_FOUND));
         return StockInfoResponseDTO.toDTO(stock);
     }
+
+    // 관심종목추가
+    @Transactional
+    public void addFavoriteStock(Short stockId) {
+        //userId 하드코딩
+        String userId = "1";
+        // 이미 추가된 관심종목인지 확인
+        if (favoriteStockRepository.existsByUserIdAndStock_StockId(userId, stockId)) {
+            return;
+        }
+        Stock stock = stockRepository.findById(stockId)
+                .orElseThrow(()-> new StockHandler(ErrorStatus.STOCK_NOT_FOUND));
+
+        FavoriteStock favoriteStock = FavoriteStock.builder()
+                .stock(stock)
+                .userId(userId)
+                .build();
+
+        favoriteStockRepository.save(favoriteStock);
+
+
+    }
+
+    //관심종목 삭제
+    @Transactional
+    public void deleteFavoriteStock(Short stockId) {
+        String userId = "1";
+        // 해당 사용자의 해당 종목 관심종목 찾기
+        FavoriteStock favoriteStock = favoriteStockRepository.findByUserIdAndStock_StockId(userId, stockId)
+                .orElseThrow(() -> new StockHandler(ErrorStatus.FAVORITE_STOCK_NOT_FOUND));
+
+        favoriteStockRepository.delete(favoriteStock);
+    }
+
 }
