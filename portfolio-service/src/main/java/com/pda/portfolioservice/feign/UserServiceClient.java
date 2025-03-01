31aad7053c4832b4cca3c0fd6a17dcf53211032a
Suyong Lee <edgarlee9364@gmail.com>
2025-03-02 02:23:19 +0900

    feat(#34): portfolio 모듈에서 openfeign 구현

--- portfolio-service/src/main/java/com/pda/portfolioservice/feign/UserServiceClient.java
+++ portfolio-service/src/main/java/com/pda/portfolioservice/feign/UserServiceClient.java
@@ -2,8 +2,12 @@ package com.pda.portfolioservice.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "userservice")
+@FeignClient(name = "user-service")
 public interface UserServiceClient {
+
+    @GetMapping("/{userId}/nickname")
+    String getNickname(@PathVariable("userId") String userId);
 
 }
