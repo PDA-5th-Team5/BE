d53464e7a6e65c3f037528b3b88109cd43bced2a
ehdms1115@naver.com <ehdms1115@naver.com>
2025-03-03 21:17:41 +0900

    "feat(#51): 경쟁사 정보조회 api"

--- stock-service/src/main/java/com/pda/stockservice/controller/StockController.java
+++ stock-service/src/main/java/com/pda/stockservice/controller/StockController.java
@@ -1,6 +1,7 @@
 package com.pda.stockservice.controller;
 
 import com.pda.stockservice.dto.response.CandleResponseDTO;
+import com.pda.stockservice.dto.response.CompetitorsResponseDTO;
 import com.pda.stockservice.dto.response.StockInfoResponseDTO;
 
 import com.pda.stockservice.feign.UserServiceClient;
@@ -51,6 +52,16 @@ public class StockController {
     public ApiResponse<CandleResponseDTO> getCandle(@PathVariable("stockId") Short stockId) {
         CandleResponseDTO candleResponseDTO = stockService.getCandle(stockId);
         return ApiResponse.onSuccess(candleResponseDTO);
+    }
+
+    //경쟁사 정보조회
+    @GetMapping("/{stockId}/competitors")
+    public ApiResponse<CompetitorsResponseDTO> getCompetitors(
+            @PathVariable("stockId") Short stockId,
+            @RequestParam(value = "sector", required = false) String sector) {
+                CompetitorsResponseDTO competitorsResponseDTO = stockService.getCompetitors(stockId , sector);
+                return ApiResponse.onSuccess(competitorsResponseDTO);
+
     }
 
     // openfeign 테스트
--- stock-service/src/main/java/com/pda/stockservice/repository/StockStatRepository.java
+++ stock-service/src/main/java/com/pda/stockservice/repository/StockStatRepository.java
@@ -2,6 +2,10 @@ package com.pda.stockservice.repository;
 
 import com.pda.stockservice.entity.StockStat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StockStatRepository extends JpaRepository<StockStat, Short> {
+    List<StockStat> findByStockIdIn(List<Short> stockIds);
 }
