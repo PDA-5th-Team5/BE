1db4108e8359f8fe8fb272b0a1f35ff43bb5d785
ehdms1115@naver.com <ehdms1115@naver.com>
2025-03-03 16:35:49 +0900

    feat(#51): 주식임계값 엔티티

--- stock-service/src/main/java/com/pda/stockservice/feign/UserServiceClient.java
+++ stock-service/src/main/java/com/pda/stockservice/feign/UserServiceClient.java
@@ -1,13 +1,22 @@
 package com.pda.stockservice.feign;
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 0741845 (feat(#51): 주식임계값 엔티티)
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "user-service")
 public interface UserServiceClient {
+<<<<<<< HEAD
 
     @GetMapping("/{userId}/nickname")
     String getNickname(@PathVariable("userId") String userId);
 }
+=======
+    @GetMapping("/user/{id}")
+    String getUserNickname(@PathVariable("userId") String userId);
+}
+>>>>>>> 0741845 (feat(#51): 주식임계값 엔티티)
