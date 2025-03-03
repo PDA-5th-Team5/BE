a2a9fef0e1f4ba44c1e23bf2ece5aa6b7cd25e92
ehdms1115@naver.com <ehdms1115@naver.com>
2025-03-03 21:45:25 +0900

    "feat(#45): 대괄호 추가"

--- stock-service/src/main/java/com/pda/stockservice/controller/StockController.java
+++ stock-service/src/main/java/com/pda/stockservice/controller/StockController.java
@@ -60,8 +60,9 @@ public class StockController {
     public ApiResponse<CompetitorsResponseDTO> getCompetitors(
             @PathVariable("stockId") Short stockId,
             @RequestParam(value = "sector", required = false) String sector) {
-                CompetitorsResponseDTO competitorsResponseDTO = stockService.getCompetitors(stockId , sector);
-                return ApiResponse.onSuccess(competitorsResponseDTO);
+        CompetitorsResponseDTO competitorsResponseDTO = stockService.getCompetitors(stockId, sector);
+        return ApiResponse.onSuccess(competitorsResponseDTO);
+    }
 
     // openfeign 테스트
     @GetMapping("/openfeign")
