a52fb467abb47efdfb6d7a96260743ec734eaa35
ehdms1115@naver.com <ehdms1115@naver.com>
2025-03-04 13:05:49 +0900

    "feat(#64): 관심종목 추가 userid 가져오기"

--- stock-service/src/main/java/com/pda/stockservice/service/StockService.java
+++ stock-service/src/main/java/com/pda/stockservice/service/StockService.java
@@ -1,8 +1,10 @@
 package com.pda.stockservice.service;
 
 import com.pda.stockservice.dto.response.CandleResponseDTO;
+import com.pda.stockservice.dto.response.CommentResponseDTO;
 import com.pda.stockservice.dto.response.CompetitorsResponseDTO;
 import com.pda.stockservice.dto.response.StockInfoResponseDTO;
+import com.pda.stockservice.repository.StockCommentRepository;
 
 public interface StockService {
     StockInfoResponseDTO getStocks(Short stockId);
@@ -11,7 +13,11 @@ public interface StockService {
     //개별종목 경쟁사 조회
     CompetitorsResponseDTO getCompetitors(Short stockId, String sector);
 
+
     //로그인기능구현 후 useId추가 예정
-    void addFavoriteStock(Short stockId);
+    void addFavoriteStock(Short stockId, String token);
     void deleteFavoriteStock(Short stockId);
+
+    //댓글
+//    CommentResponseDTO get
 }
