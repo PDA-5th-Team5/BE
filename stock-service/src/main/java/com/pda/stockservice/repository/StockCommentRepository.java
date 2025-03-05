bbf3a1d8455a19011dd3b2370f956b1d164b4a92
ehdms1115@naver.com <ehdms1115@naver.com>
2025-03-05 17:35:55 +0900

    "feat(#79): StockCommentRepository충돌해결"

--- stock-service/src/main/java/com/pda/stockservice/repository/StockCommentRepository.java
+++ stock-service/src/main/java/com/pda/stockservice/repository/StockCommentRepository.java
@@ -3,8 +3,10 @@ package com.pda.stockservice.repository;
 import com.pda.stockservice.entity.StockComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface StockCommentRepository extends JpaRepository<StockComment, Long > {
     List<StockComment> findByStock_StockIdOrderByCreatedAtDesc(Short stockId);
+    Optional<List<StockComment>> findByUserId(String userId);  // userId로 댓글 조회
 }
