8f2c0c6ba877470783fb5495f8aa2fd491c5fe16
ehdms1115@naver.com <ehdms1115@naver.com>
2025-03-06 13:05:58 +0900

    "feat(#93): redis 숫자 앞0 허용 설정"

--- stock-service/src/main/java/com/pda/stockservice/config/RedisConfig.java
+++ stock-service/src/main/java/com/pda/stockservice/config/RedisConfig.java
@@ -6,6 +6,9 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonParser;
+
 
 @Configuration
 public class RedisConfig {
@@ -22,8 +25,15 @@ public class RedisConfig {
         // 모든 값은 JSON 직렬화**
         Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer =
                 new Jackson2JsonRedisSerializer<>(Object.class);
+
+        // ObjectMapper 설정 추가
+        ObjectMapper objectMapper = new ObjectMapper();
+        // 숫자 앞의 0 허용 설정
+        objectMapper.configure(JsonParser.Feature.ALLOW_NUMERIC_LEADING_ZEROS, true);
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer2 =
+                new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
         template.setValueSerializer(jackson2JsonRedisSerializer);
-        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.setHashValueSerializer(jackson2JsonRedisSerializer2);
 
         template.afterPropertiesSet();
         return template;
