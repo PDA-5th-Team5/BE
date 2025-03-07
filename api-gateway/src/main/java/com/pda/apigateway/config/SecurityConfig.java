4c93d20c6236c98eed7535aacd4a8c26d492f545
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-03-07 12:51:13 +0900

    test: front CORS 오류 해결 테스트

--- api-gateway/src/main/java/com/pda/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/pda/apigateway/config/SecurityConfig.java
@@ -15,7 +15,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 @Configuration
@@ -58,5 +63,28 @@ public class SecurityConfig {
 
 
         return http.build();
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // 허용할 도메인 설정
+        config.setAllowedOrigins(Arrays.asList(
+                "https://snowper.shop",
+                "http://localhost:5173"
+        ));
+
+        // 허용할 HTTP 메서드
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        // 허용할 헤더
+        config.setAllowedHeaders(Arrays.asList("*"));
+
+        // 인증정보 포함 여부
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
