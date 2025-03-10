9bf1ee4a164d66e00fc2181629319610463dbc9f
Pasak22 <125808024+Pasak22@users.noreply.github.com>
2025-03-10 19:52:34 +0900

    fix(#120): cookie secure 옵션 제거

--- user-service/src/main/java/com/pda/userservice/jwt/LoginFilter.java
+++ user-service/src/main/java/com/pda/userservice/jwt/LoginFilter.java
@@ -132,7 +132,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         cookie.setMaxAge(24 * 60 * 60);
         cookie.setPath("/");
         cookie.setHttpOnly(true);
-        cookie.setSecure(true); // HTTPS 환경에서만 동작 (개발 환경에서는 주석처리 가능)
+//        cookie.setSecure(true); // HTTPS 환경에서만 동작 (개발 환경에서는 주석처리 가능)
 
         // 기존 Cookie API는 SameSite 속성을 지원하지 않으므로, 헤더 문자열에 직접 추가합니다.
         String cookieHeader = String.format("%s=%s; Max-Age=%d; Path=%s; HttpOnly; Secure; SameSite=None",
