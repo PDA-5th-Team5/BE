8edf6dec001ce644f1f61efc2a1aba1bfbefbcc7
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-03-10 14:08:01 +0900

    fix: 종목 전체 개수 오류 수정, 텔레그램 토큰 환경변수 수정

--- portfolio-service/src/main/java/com/pda/portfolioservice/PortfolioServiceApplication.java
+++ portfolio-service/src/main/java/com/pda/portfolioservice/PortfolioServiceApplication.java
@@ -1,6 +1,5 @@
 package com.pda.portfolioservice;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -14,11 +13,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class PortfolioServiceApplication {
     // .env 파일을 static 블록에서 로드하여 환경 변수로 설정
-    static {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(PortfolioServiceApplication.class, args);
     }
