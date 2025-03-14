a03f4cdd83b9eb7edd63c5e99d2cd9df7eb3f57e
Han_Yeong_Jang <qpwisu@kookmin.ac.kr>
2025-03-14 18:08:18 +0900

    feat: 캐시 로그 활성화

--- stock-service/src/main/java/com/pda/stockservice/StockServiceApplication.java
+++ stock-service/src/main/java/com/pda/stockservice/StockServiceApplication.java
@@ -3,12 +3,14 @@ package com.pda.stockservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableFeignClients
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class StockServiceApplication {
 
 	public static void main(String[] args) {
